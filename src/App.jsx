0ccdc7a1d300f6e192786324608f601cf488ecae
Florin12er <142166195+Florin12er@users.noreply.github.com>
2024-01-14 19:11:22 +0100

    add the ability to add a goal and add a description about me

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,63 @@ import PropTypes from "prop-types";
 function Sebastian({ bgColor }) {
   return (
     <div>
-      <p className={`text-3xl font-bold ${bgColor}`}>hello world</p>
+      <p className={`text-3xl font-bold ${bgColor} w-1/2`}>hello world</p>
     </div>
   );
 }
+
+function Present() {
+  return (
+    <div>
+      <p className="text-3xl font-bold bg-violet-500 mt-5 w-1/2">
+        hello my name is sebastian I am a student in cs and i love coding
+      </p>
+    </div>
+  );
+}
+
+function FavoriteFood() {
+  return (
+    <div className="text-3xl font-bold bg-orange-500 w-1/2">
+      <p>my favorite food is pasta and broccoli</p>
+    </div>
+  );
+}
+
+function AddGoal({ onAddGoal }) {
+  const [newGoal, setNewGoal] = useState("");
+
+  const handleInputChange = (event) => {
+    setNewGoal(event.target.value);
+  };
+
+  const handleAddGoal = () => {
+    onAddGoal(newGoal);
+    setNewGoal("");
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Enter new goal"
+        className="w-1/2 h-11 text-3xl font-bold p-1 mt-5 bg-gray-400 placeholder:text-black "
+        value={newGoal}
+        onChange={handleInputChange}
+      />
+      <button
+        onClick={handleAddGoal}
+        className="text-3xl font-bold bg-green-500 p-1 ml-2 "
+      >
+        Add Goal
+      </button>
+    </div>
+  );
+}
+
+AddGoal.propTypes = {
+  onAddGoal: PropTypes.func.isRequired,
+};
 
 Sebastian.propTypes = {
   bgColor: PropTypes.string.isRequired,
@@ -15,6 +68,7 @@ Sebastian.propTypes = {
 
 function App() {
   const [bgColor, setBgColor] = useState("bg-red-500");
+  const [goals, setGoals] = useState([]);
 
   const showAlert = () => {
     alert("Why did you press me!!! I am now a different color");
@@ -23,12 +77,30 @@ function App() {
     );
   };
 
+  const handleAddGoal = (newGoal) => {
+    setGoals((prevGoals) => [...prevGoals, newGoal]);
+  };
+
   return (
     <div>
       <Sebastian bgColor={bgColor} />
-      <button className="text-3xl font-bold bg-blue-500" onClick={showAlert}>
+      <button
+        className="text-3xl font-bold bg-blue-500 p-1"
+        onClick={showAlert}
+      >
         Press me
       </button>
+      <Present />
+      <FavoriteFood />
+      <AddGoal onAddGoal={handleAddGoal} />
+
+      <div>
+        {goals.map((goal, index) => (
+          <p key={index} className="text-3xl font-bold bg-blue-900 mt-2 w-1/2">
+            {goal}
+          </p>
+        ))}
+      </div>
     </div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
+import { createRoot } from "react-dom/client";
 import React from "react";
-import ReactDOM from "react-dom";
 import { App } from "./App.jsx";
-import "./output.css";
+import "./css/output.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
